d3b691bdf9b9abe825877db76ee9417df52e5f94
kurtasov <kurtasov@users.noreply.github.com>
2022-12-08 15:28:27 +0300

    Added patient age, ACK

--- receiver.py
+++ receiver.py
@@ -1,5 +1,16 @@
 import socketserver
 from socket import SOL_SOCKET, SO_REUSEPORT
+import datetime
+
+class Patient:
+    def __init__(self, hl7data):
+        self.name = hl7data['PID'][5]
+        self.date_of_birth = datetime.datetime.strptime(hl7data['PID'][7], '%Y%m%d')
+
+    def get_age(self):
+        age = datetime.datetime.now() - self.date_of_birth
+        return int(age.days/365)
+
 
 class HL7Message:
     def __init__(self, msg=''):
@@ -15,6 +26,10 @@ class HL7Message:
 
         return self.segments
 
+    def generate_ack(self):  # TODO: генерировать сообщение с текущей датой и ссылкой на принятое сообщение
+        return f'\x0bMSH|^~\\&|Main_HIS|XYZ_HOSPITAL|iFW|ABC_Lab|20160915003015||ACK|9B38584D|P|2.6.1|\rMSA|AA|9B38584D|Everything was okay dokay!|\r\x1c\r'
+
+
 class MLLPHandler(socketserver.BaseRequestHandler):
     def handle(self):
         while True:
@@ -27,9 +42,16 @@ class MLLPHandler(socketserver.BaseRequestHandler):
             if self.data.decode('utf-8')[0:5] == '\x1c\r':
                 hl7_message.msg = hl7_message.msg.splitlines()[1:]
                 print(f"Получено сообщение от {self.client_address[0]}: {hl7_message.msg}")
-                s = hl7_message.parse()
-                # print(s)
-                print(s['PV1'][7])
+
+                try:
+                    hl7_data = hl7_message.parse()
+                except:  # TODO: Реализовать обработку ошибок
+                    print("Ошибка парсинга сообщения")
+                else:
+                    self.request.sendall(hl7_message.generate_ack().encode('utf-8'))
+                    pt = Patient(hl7_data)
+                    print(f'Поступил пациент {pt.name} возрастом {pt.get_age()} лет')
+
 
 
 class MyServer(socketserver.ThreadingMixIn, socketserver.TCPServer):
